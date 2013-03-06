3179556b7d4be39ce4d2a9cf2cb1944a163fe98d
Eli Fatsi <eli.fatsi@viget.com>
2013-03-06 08:00:01 -0700

    Scoping on environment for fb key/secret

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,14 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-           :scope => 'user_photos, friends_photos', :display => 'popup',
-           redirect_uri:'http://localhost:3000/auth/facebook/callback'
+
+  if Rails.env == 'development' || Rails.env == 'test'
+    provider :facebook, ENV['DEV_FACEBOOK_KEY'], ENV['DEV_FACEBOOK_SECRET'],
+             :scope => 'user_photos, friends_photos', :display => 'popup',
+             redirect_uri:'http://localhost:3000/auth/facebook/callback'
+  else
+    # Production
+    provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
+             :scope => 'user_photos, friends_photos', :display => 'popup',
+             redirect_uri:'http://localhost:3000/auth/facebook/callback'
+  end
+
 end
